d50241b3d0cec46e017f942ba604969bf3878d9b
martin jerez <martincho111986@gmail.com>
2020-08-22 03:00:55 -0300

    primer commit backend

---
+++ routes/tareas.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const tareaController = require("../controllers/tareaController");
+const auth = require("../middleware/auth");
+const { check } = require("express-validator");
+
+//crear una tarea
+//api/tareas
+router.post(
+  "/",
+  auth,
+  [
+    check("nombre", "El Nombre es Obligatorio").not().isEmpty(),
+    check("proyecto", "El Proyecto es Obligatorio").not().isEmpty(),
+  ],
+  tareaController.crearTarea
+);
+
+router.get("/", auth, tareaController.obtenerTareas);
+
+//actualizar tarea
+router.put("/:id", auth, tareaController.actualizarTarea);
+
+//eliminar una tarea
+router.delete("/:id", auth, tareaController.eliminarTarea);
+
+module.exports = router;
