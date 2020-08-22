03b72b17d799257a075d7b4af94d440be4f05fd2
martin jerez <martincho111986@gmail.com>
2020-08-22 03:45:13 -0300

    subiendo cambios

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //PUERTO DE LA APP
-const PORT = process.env.PORT  || 8080;
+const port = process.env.PORT  || 8080;
 
 //importar rutas
 app.use("/api/usuarios", require('./routes/usuarios'));
@@ -24,6 +24,6 @@ app.use("/api/proyectos", require('./routes/proyectos'));
 app.use("/api/tareas", require('./routes/tareas'));
 
 //arrancar la app
-app.listen(PORT, () =>{
+app.listen(port, '0.0.0.0', () =>{
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 })
